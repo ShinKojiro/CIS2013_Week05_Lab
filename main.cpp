523c1dd0a0d39170d901ef76b7e7fc230cb28a16
ShinKojiro <42621442+ShinKojiro@users.noreply.github.com>
2019-02-20 18:38:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,34 @@
 using namespace std;
 
 int main(){
-    
+
+    int len = 3;
+
+    // List of students in our class
+    int student[len];
+
+    // int bob = 90;
+    // int tom = 15;
+    // int jill = 99;
+
+    for(int i = 0; i < len; i++){
+    cout << "Enter Student " << i + 1 << "'s grade: ";
+    cin >> student[i];  
+    }
+
+    // cout << "Enter Bobs grade ";
+    // cin >> student[0];
+    // cout << "Enter Toms grade ";
+    // cin >> student[1];
+    // cout << "Enter jill's grade ";
+    // cin >> student[2];
+
+    // cout << "Bob grade is: " << student[0] << endl;
+    // cout << "Tom grade is: " << student[1] << endl;
+    // cout << "Jill grade is: " << student[2] << endl;
+
+    for(int i = 0; i < len; i++){
+        cout << "Student " << i + 1 << "'s grade is " << student[i] << endl;
+    }
+
 }
