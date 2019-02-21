cb5579cb89bb48960cb4808d2b48eeda9abb442c
ShinKojiro <42621442+ShinKojiro@users.noreply.github.com>
2019-02-20 20:31:03 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,48 +3,36 @@
 
 using namespace std;
 
+const int MAX = 30;
+int student[MAX];
+string name[MAX];
+
+void makeStudent(int x){
+    cout << "Enter Student's name: ";
+    cin >> name[x];
+    cout << "Enter Student " << x + 1 << "'s grade: ";
+    cin >> student[x]; 
+}
+
+void printGrade(int y){
+    cout << name[y] << "'s grade is " << student[y] << endl;
+}
+
 int main(){
 
-    const int MAX = 30;
+    
 
     int LEN;
     cout << "How many students are in this class? ";
     cin >> LEN;
-    
-    
-
-    // List of students in our class
-    int student[MAX];
-    // string name[LEN] = { "Bob", "Tom", "Jill" };
-    string name[MAX];
-
-    // int bob = 90;
-    // int tom = 15;
-    // int jill = 99;
 
     for(int i = 0; i < LEN; i++){
-        cout << "Enter Student's name: ";
-        cin >> name[i];
-        cout << "Enter Student " << i + 1 << "'s grade: ";
-        cin >> student[i];  
+        makeStudent(i);
     }
     cout << endl;
-    // cout << "Enter Bobs grade ";
-    // cin >> student[0];
-    // cout << "Enter Toms grade ";
-    // cin >> student[1];
-    // cout << "Enter jill's grade ";
-    // cin >> student[2];
-
-    // cout << "Bob grade is: " << student[0] << endl;
-    // cout << "Tom grade is: " << student[1] << endl;
-    // cout << "Jill grade is: " << student[2] << endl;
-
-    // for(int i = 0; i < LEN; i++){
-    //     cout << name[i] << " " << i + 1 << "'s grade is " << student[i] << endl;
-    // }
+    
     for(int i = 0; i < LEN; i++){
-        cout << name[i] << "'s grade is " << student[i] << endl;
+        printGrade(i);
     }
 
 }
